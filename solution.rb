cea9848b967e4ad074a4f5a9d6159901c2d700d8
ingRamiroContreras <ramirocontreras65@gmail.com>
2019-03-25 11:32:08 -0500

    sinatra3

--- solution.rb
+++ solution.rb
@@ -1,19 +1,28 @@
 require "sinatra"
 
-get '/' do
-    if params[:nombre] == ''
-        @nombre = "Desconocido"
-    else
-        @nombre = params[:nombre].capitalize
-    end
-    erb :index
+#get '/' do
+#    if params[:nombre] == ''
+#        @nombre = "Desconocido"
+#    else
+#        @nombre = params[:nombre].capitalize
+#    end
+#    erb :index
+#end
+
+get "/" do
+  erb :index
 end
 
-get '/makers/:nombre' do
-    if params[:nombre] == ''
-        @nombre = "Desconocido"
-    else
-        @nombre = params[:nombre].capitalize
-    end
-    erb :index
+post "/saludar" do
+  @nombre = params["nombre"]
+  erb :saludar
+end
+
+get "/makers/:nombre" do
+  if params[:nombre] == ""
+    @nombre = "Desconocido"
+  else
+    @nombre = params[:nombre].capitalize
+  end
+  erb :index
 end
